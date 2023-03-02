0e3d69c171c7bd1ef510ae59bcd2594e25acf52e
chubi-x <chubixax@protonmail.com>
2023-03-02 12:24:58 +0000

    feat:create models

---
+++ app/Models/Image.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Image extends Model
+{
+    use HasFactory;
+    public function imageable():MorphTo{
+        return $this->morphTo(); //single image model has one-to-one relationship with User and Room model
+    }
+}
