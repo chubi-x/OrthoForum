f54529234feffb93206dd57c449575cbb00e0772
chubi-x <chubixax@protonmail.com>
2023-03-02 22:55:35 +0000

    chore: isadmin shouldnt be fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class User extends Model
 {
     use HasFactory;
-    protected $fillable = ['fullname','username','email','password','isAdmin'];
+    protected $fillable = ['fullname','username','email','password'];
 
     // get user profile pic
     public function profile_pic():MorphOne{
