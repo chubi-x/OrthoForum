d2c3d12479982d567bc7f17d685eb6aeef87588f
chubi-x <chubixax@protonmail.com>
2023-04-06 18:36:03 +0100

    chore: create layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>
+       OrthoForum -  @yield("title")
+    </title>
+</head>
+
+@yield('content')
+</html>
---
+++ resources/views/layouts/error.blade.php
@@ -0,0 +1,10 @@
+@if($errors->any())
+    <div>
+        Errors:
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
