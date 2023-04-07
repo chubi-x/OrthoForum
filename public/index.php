1e616b0bd4acb6389b02674068ba8f10a79a3e2d
chubi-x <chubixax@protonmail.com>
2023-04-07 17:47:58 +0100

    fix cors

--- public/index.php
+++ public/index.php
@@ -45,7 +45,9 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers:*");
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
