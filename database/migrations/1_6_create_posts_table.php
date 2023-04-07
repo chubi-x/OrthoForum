153af6e1064b1112b1e1f4bce244741554845ec0
chubi-x <chubixax@protonmail.com>
2023-04-07 21:58:03 +0100

    feat: add like column to post

--- database/migrations/1_6_create_posts_table.php
+++ database/migrations/1_6_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->longText("text");
+            $table->bigInteger("likes")->default(0);
             $table->foreignId("member_id")->constrained()->restrictOnDelete()->cascadeOnUpdate();
             $table->foreignId('room_id')->nullable()->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
