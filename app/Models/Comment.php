9970d961ade18c36e0074eb670561bd2594e0ab0
chubi-x <chubixax@protonmail.com>
2023-04-09 17:16:24 +0100

    feat: commenting on comments too complex to implement now

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
@@ -15,8 +14,5 @@ class Comment extends Model
     }
     public function post():BelongsTo{
         return $this->belongsTo(Post::class);
-    }
-    public function comments():HasMany{
-        return $this->hasMany(Comment::class);
     }
 }
